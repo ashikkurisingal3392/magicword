7c009b184f98a8281f21d0f3b8ed7d0a58daf132
ashik <ashikkurisingal@gmail.com>
2025-04-28 11:48:46 +0100

    add a ascii art into the horoscope message and changes some style in message div

--- magicwords.js
+++ magicwords.js
@@ -7,7 +7,8 @@ const randomNumber =(words) => {
 
 const horoscopeData ={moods :['Exciting','mysterious ','challenging','peacefull','calm','playfull','intense','hopefull','vibrant'],
     actions :['heal old wounds','follow your curiosity','trust your instincts','plant seeds for future success','seek clarity before committing','explore a new passion','make peace with the past'],
-    advice : ['luck favors your boldness','the answers you seek are already within you','you are standing at the edge of something beautiful','your dreams are planting roots today','kindness will open unexpected doors','the world is ready for your light']
+    advice : ['luck favors your boldness','the answers you seek are already within you','you are standing at the edge of something beautiful','your dreams are planting roots today','kindness will open unexpected doors','the world is ready for your light'],
+    asciiArt: [`><(((('>`,`=^.^=`,`@}-;-'---`,`(•ө•)`,`<:3 )~~`]
                      };
 
 const magicWord = (horoscopeObj) => {
@@ -22,14 +23,17 @@ const magicWord = (horoscopeObj) => {
         switch(horoscope){
 
             case "moods":
-                horoscopeMessage.push(`" Today feels ${horoscopeObj[horoscope][horoscopeIndex]} `);
+                horoscopeMessage.push(`"  Today feels ${horoscopeObj[horoscope][horoscopeIndex]} `);
                 break;
             case "actions":
-                horoscopeMessage.push(`- its prefect time to ${horoscopeObj[horoscope][horoscopeIndex]}`);
+                horoscopeMessage.push(`- its prefect time to ${horoscopeObj[horoscope][horoscopeIndex]} `);
                 break;
             case "advice":
-                horoscopeMessage.push(`and ${horoscopeObj[horoscope][horoscopeIndex]} "`);    
+                horoscopeMessage.push(`and ${horoscopeObj[horoscope][horoscopeIndex]}  "`);    
                 break;
+            case "asciiArt":
+            horoscopeMessage.push(`${horoscopeObj[horoscope][horoscopeIndex]} `);    
+            break;
             default :
                  horoscopeMessage.push(`Nothing to display.try again`);
 
@@ -40,9 +44,11 @@ const magicWord = (horoscopeObj) => {
 }
 const magicWordShow = () => {
     const magicWordArray= magicWord(horoscopeData);
-   const magicWordString= magicWordArray.join('');
-    console.log(magicWordString);
+    const allButLast = magicWordArray.slice(0,-1).join('');
+   const asciiArt= magicWordArray[magicWordArray.length - 1];
+   const magicWordString = allButLast +"\n"+asciiArt;
+     // console.log(magicWordString);
     //To attach the random choosed words to a div
-    document.getElementById('horoscope-message').innerHTML=magicWordString;
+   document.getElementById('horoscope-message').innerHTML=magicWordString.replace(/\n/g,'<br>');
 }
 //magicWordShow();
