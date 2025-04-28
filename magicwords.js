c32d67563bb641c7e20d67cf764f99b9a54259a4
ashik <ashikkurisingal@gmail.com>
2025-04-28 10:09:53 +0100

    change the horoscope words into an object and randomnly shows the horoscope in a single string.some changes on css is done.

--- magicwords.js
+++ magicwords.js
@@ -2,22 +2,47 @@
 
 const randomNumber =(words) => {
 
-    return Math.floor(Math.random()*words.length);
+    return Math.floor(Math.random()*words);
 }
 
+const horoscopeData ={moods :['Exciting','mysterious ','challenging','peacefull','calm','playfull','intense','hopefull','vibrant'],
+    actions :['heal old wounds','follow your curiosity','trust your instincts','plant seeds for future success','seek clarity before committing','explore a new passion','make peace with the past'],
+    advice : ['luck favors your boldness','the answers you seek are already within you','you are standing at the edge of something beautiful','your dreams are planting roots today','kindness will open unexpected doors','the world is ready for your light']
+                     };
 
-const moods = ['Exciting','mysterious ','challenging','peacefull','calm','playfull','intense','hopefull','vibrant'];
-const actions =['heal old wounds','follow your curiosity','trust your instincts','plant seeds for future success','seek clarity before committing','explore a new passion','make peace with the past'];
-const advice =['luck favors your boldness','the answers you seek are already within you','you are standing at the edge of something beautiful','your dreams are planting roots today','kindness will open unexpected doors','the world is ready for your light'];
+const magicWord = (horoscopeObj) => {
+    let horoscopeMessage =[];
+ // find the object values and random arrays inside object and attach to new array. 
+    for(let  horoscope in horoscopeObj)
 
-const magicWord = (moods,actions,advice) => {
-    const moodChoose =moods[randomNumber(moods)];
-    const actionChoose =actions[randomNumber(actions)];
-    const adviceChoose = advice[randomNumber(advice)];
-    const horoscopeSentence =`Today feels ${moodChoose} - its prefect time to ${actionChoose} and ${adviceChoose}`;
-    //return horoscopeSentence;
-    document.getElementById('horoscope-message').innerHTML=horoscopeSentence;
+    {
+        // create a  random array number
+        let horoscopeIndex =randomNumber(horoscopeObj[horoscope].length)
+
+        switch(horoscope){
+
+            case "moods":
+                horoscopeMessage.push(`" Today feels ${horoscopeObj[horoscope][horoscopeIndex]} `);
+                break;
+            case "actions":
+                horoscopeMessage.push(`- its prefect time to ${horoscopeObj[horoscope][horoscopeIndex]}`);
+                break;
+            case "advice":
+                horoscopeMessage.push(`and ${horoscopeObj[horoscope][horoscopeIndex]} "`);    
+                break;
+            default :
+                 horoscopeMessage.push(`Nothing to display.try again`);
+
+        }
+
+    }
+    return horoscopeMessage;
 }
 const magicWordShow = () => {
-    magicWord(moods,actions,advice);
+    const magicWordArray= magicWord(horoscopeData);
+   const magicWordString= magicWordArray.join('');
+    console.log(magicWordString);
+    //To attach the random choosed words to a div
+    document.getElementById('horoscope-message').innerHTML=magicWordString;
 }
+//magicWordShow();
